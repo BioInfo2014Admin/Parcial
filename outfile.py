e5633607e9af5eff9162b0cff41d6e32e49a8e09
Michay Diez <rockeargesell@gmail.com>
2014-09-23 15:01:28 -0300

    Update outfile.py

--- outfile.py
+++ outfile.py
@@ -1,28 +1,44 @@
 import sys
+import numpy as np
+from Bio import SeqIO
+import Bio
 
-def outfile(n, f):
+def _outfile(m, n, f):
     
-    """The function returns a file with selected name and format.
+    """The function returns the alignments in a file with selected name and format.
     
-    Via stdout, the function creates a file where printed arguments 
-    are saved instead of printing in screen.
+    First, it converts the alignment matrix to seq element. Via stdout, the function
+    creates a file where printed arguments are saved instead of printing in screen.
     
     Arguments:
+    -m- alignment matrix 
     -n- the name of the file
     -f- the file format
     
     Example:
     >>>import sys
-    >>>my_seq = ("AAACTGCCA")
-    >>>outfile("example.fasta","fasta")
-    >>>print(my_seq)"""
+    >>>import numpy as np
+    >>>import Bio
+    >>>from Bio import SeqIO
+    >>>outfile(matrix,"example.fasta","fasta")"""
+    
+    sys.stdout = open (n, "w")
     
     if f=="fasta":
-        pass
+        for i in range(0,len(m)):
+            seq_list = m[i].tolist()
+            S = ""
+            R = S.join(seq_list)
+            Seq1 = Bio.Seq.Seq(R)
+            print(">"+ID[i]+"\n"+Seq1)
+            
     elif f=="phylip":
-        pass
-    elif f=="gbk":
-        pass
+        print(str(len(m))+" "+str(len(m[0])))
+        for i in range (0,len(m)):
+            seq_list = m[i].tolist()
+            S = ""
+            R = S.join(seq_list)
+            Seq1 = Bio.Seq.Seq(R)
+            print(ID[i]+" "+Seq1)
     else:
         raise Exception("Wrong format. Choose accepted format.")
-    sys.stdout = open (n, "w")
