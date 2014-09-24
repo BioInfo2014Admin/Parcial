2f9e9f4be748cf6d38c45d1c23e77e2c2715c1d9
Leonel <leonelatm44@gmail.com>
2014-09-23 22:06:04 -0300

    Update Parcial.py

--- Parcial.py
+++ Parcial.py
@@ -8,7 +8,7 @@ The second way is introducing a list of tuples as an argument.
 
 """
 
-def parcial(dirseq="",protalign=True,outputname="parcial",tupla="",outputnamealign="alignment"):
+def parcial(dirseq="",protalign=True,outputname="parcial.fasta",tupla="",outputnamealign="alignment.fasta",codon_table=1):
     """ aca van los ejemplos
     """
     import Infile
@@ -17,22 +17,27 @@ def parcial(dirseq="",protalign=True,outputname="parcial",tupla="",outputnameali
     import removestops
     import alignproteins
     import outfile
-#primero transformamos el input a fasta
+    import ID
+#primero transformamos el input a una matriz
+    ID = ID._ID(dirseq)
     array =Infile._input(dirseq)
 #enviamos al modulo que lo corta
+    
     interestarray = Selectingbyzones._zoneselector(array,tupla)
 #enviamos al modulo que limpia gaps
+    
     nogapsarray = limpiezagaps._limpiezagaps(interestarray)
 #enviamos al modulo que limpia stops
-    finalarray = removestops._remove_stops(nogapsarray)
+    finalarray = removestops._remove_stops(nogapsarray,codon_table)
 #cuando corresponda, generamos el alineamiento de proteinas
     if protalign == True:
-        arrayprotalign = alignproteins._alinprot(finalarray)
+        arrayprotalign = alignproteins._alinprot(finalarray,codon_table)
 #transformamos al formato deseado por el user y lo enviamos a un archivo.
-    outfile._outfile(matrizlimpia,outputname,outputformat)
+
+    outfile._outfile(finalarray,outputname,ID)
 
     if protalign == True:
-    	outfile._outfile(arrayprotalign,outputnamealign,outputformatalign)
+    	outfile._outfile(arrayprotalign,outputnamealign)
                
             
         
@@ -47,10 +52,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ACA VA LA DESCRIPCION DEL PROGRAMA')
     parser.add_argument('-seqdir',  type=str, help='input name')
     parser.add_argument('-protalign', action='store_const', const = True, default = False, help='if True will return cleaned protein aligment ')
-    parser.add_argument('-outputname', type=str, help='output name', default = "parcial")
-    parser.add_argument('-outputnamealign', type=str, help='output name for the protein aligment', default = "aligment")
-    parser.add_argument('-tupla', type=tuple, help='lista de tuplas que contiene las zonas de interes',default="")
+    parser.add_argument('-outputname', type=str, help='output name', default = "parcial.fasta")
+    parser.add_argument('-outputnamealign', type=str, help='output name for the protein aligment', default = "aligment.fasta")
+    parser.add_argument('-tupla', type=tuple, help='lista de tuplas que contiene las zonas de interes')
+    parser.add_argument('-codon_table', type=int, help='number of the codon table to use', default = 1)
 #esto carga los argumentos en una variable    
     args = parser.parse_args()
 #esto envia los argumentos a la funcion parcial, coimo argumentos para la funcion.    
-    parcial(args.seqdir,args.protalign,args.outputname,args.tupla,args.outputnamealign)
+    parcial(args.seqdir,args.protalign,args.outputname,args.tupla,args.outputnamealign,args.codon_table)
