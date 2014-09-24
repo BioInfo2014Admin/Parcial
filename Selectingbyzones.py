b7d1e363ddbac269d5c727c3e4de909d80a7012a
AIRibone <andresribone1994@gmail.com>
2014-09-23 21:05:46 -0300

    Update Selectingbyzones.py
    Todo lo que estuve haciendo, igual no está terminado, pero funciona

--- Selectingbyzones.py
+++ Selectingbyzones.py
@@ -2,55 +2,42 @@
 #Hola, la primer función es necesaria para la segunda, y probablemente les sirva a ustedes.
 #
 #
-def _Recepcion(Inputfasta):
-    """
-    str--> matrix
-    Recibe la dirección del archivo de alineamiento en fasta (Inputfasta) como un string, parsea y toma las secuencias. Con estas genera una matriz donde las columnas corresponden a las columnas de un alineamiento y las filas a cada secuencia.
-    
-    Parametros
-    ---------
-    Inputfasta : Debe ser la dirección de un archivo de alineamiento en .fasta, debe ingresarse entre comillas.
-    
-    """
-    from Bio import SeqIO
-    import numpy as np
-    Archivoparseado = SeqIO.parse(Inputfasta,"fasta")
-    Listasecuenciasinput=[record for record in Archivoparseado] #genero listas con secuencias parseadas
-    Paraformarmatriz=[[i for i in str(Listasecuenciasinput[j].seq)] for j in range(0,len(Listasecuenciasinput))] #a partir de la lista anterior genero una lista que contiene las secuencias en formato lista: cada nucleótido es un elemento. Me queda algo así: [[A,T,G],[G,T,T],[T,C,A]]
-    Arrayalineamiento=np.array(Paraformarmatriz) #la lista de listas anterior la puedo utilizar directamente para formar la matriz que quiero, donde las columnas quedan alineadas.
-    return(Arrayalineamiento) #expulsa el array
-#
-#
-def _Selecciondezonas(Inputfasta, Inseleccion=None):
-    """
-    Construye un array (¿o dataframe?) con las zonas seleccionadas
-    
-    Parameters
-    ----------
-    Inputfasta : 
-                Debe ser la dirección de un archivo de alineamiento en .fasta, debe ingresarse entre comillas.
-    Inseleccion : Lista de tuplas
-        Cada tupla de la lista debe poseer con los # de las columnas que franquean los bloques que quiero seleccionar 
-        Ejemplo Inseleccion=[
-    
-    Returns
-    -------
-    Un array (#o dataframe?)
-    """
+def _zoneselector(input_array, tuple_selection=None, selection_01=None):
+    if tuple_selection!=None and selection_01!=None:
+        raise Exception("Double zone selection. Choose only one way of zone selection")
     import numpy as np
     import pandas as pd
-    Arrayalineamiento=_Recepcion(Inputfasta)
-    if Inseleccion != None:
-        Lista=[]
-        Arrayseleccion=np.zeros((len(Arrayalineamiento),0))
-        for i in range(0,len(Inseleccion)):
-            Arrayseleccion=np.hstack([Arrayseleccion, Arrayalineamiento[:,Inseleccion[i][0]:Inseleccion[i][1]]])
-            for j in range(Inseleccion[i][0],Inseleccion[i][1]):
-                Lista.append(j)
-        #return(Arrayseleccion)
-        return(pd.DataFrame(Arrayseleccion,columns=Lista))
-    else:
-        #return(Arrayalineamiento)
-        return(pd.DataFrame(Arrayalineamiento))
+    import scipy
+    if tuple_selection != None and selection_01==None:
+        selection_array=np.zeros((len(input_array),0))
+        for i in range(0,len(tuple_selection)):
+            R=tuple_selection[i][1]-tuple_selection[i][0] 
+            if R%3!=0 or tuple_selection[i][0]%3!=0:
+                raise Exception("Warning, your tuple selection affects the reading frame")
+#                return("Warning, your tuple selection affects the reading frame"+"\n"+"Continue aniways?[Y/N]")
+#                ans=input()
+#                if ans==["Y"]:
+#                    pass
+            selection_array=np.hstack([selection_array, input_array[:,tuple_selection[i][0]:tuple_selection[i][1]]])
+        return(selection_array)
+    elif tuple_selection==None and selection_01!=None:
+        handle=open(selection_01,"r")
+        zero_ones=[i for i in handle.read()]
+        handle.close
+        for i in range(0,len(zero_ones)-1):
+            try:
+                r=int(zero_ones[i])
+            except:
+                raise Exception("'selection_01' file can only contain 0 or 1")
+            if r!=0 and r!=1:
+                raise Exception("'selection_01' file can only contain 0 or 1")
+        columns_to_delete=[]
+        for i in range(0, len(zero_ones)):
+            auxlist1=[j for j, r in enumerate(zero_ones) if r=="0"]
+            columns_to_delete.extend(auxlist1)
+        selection_array=scipy.delete(input_array,columns_to_delete,1) ###
+        return(selection_array)
+    elif tuple_selection==None and selection_01==None:
+        return(input_array)
 #
 #
