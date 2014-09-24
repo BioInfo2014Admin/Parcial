5680f355fcd84eb4f73247a421ad81051998575e
Josefinita <mariajosefinadoffo@hotmail.com>
2014-09-23 22:46:59 -0300

    Update alignproteins.py

--- alignproteins.py
+++ alignproteins.py
@@ -1,13 +1,19 @@
-def _alignproteins(NM):
-    """This function translates a nucelotide matrix already  aligned
-    It goes along the rows and columns and list their elements, transforms them into a Biopython's sequence and eventually translate them to protein
+def _alignproteins(NM, codtable):
+    
+    """T
+    This function translates a nucelotide matrix already  aligned
+    It goes along the rows and columns and list their elements, 
+    transforms them into a Biopython's sequence and eventually translate them to protein
     
     Argument:
+    
     -NM- Nucleotide matrix
+    -codtable- CodonTable from IUPAC
     
     Example:
+    
     Alignproteinsmatrix=np.array([["A","C","G","G","A","A"],
-                              ["A","C","G","T","C","T"]])
+                                 ["A","C","G","T","C","T"]])
     for i in range(0,len(Alignproteinsmatrix)):
         list_1=Alineproteinsmatrix[i].tolist()
         H=""
@@ -18,7 +24,9 @@ def _alignproteins(NM):
         TE
         TS
         """
+    from Bio.Data import CodonTable 
     import numpy as np
+    CodonTable.unambiguous_dna_by_id[codtable]
     for i in range (0,len(NM)):
         list_1=NM[i].tolist()
         H=""
