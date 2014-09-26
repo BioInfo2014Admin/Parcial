07e91c81f25cbf988028b408f40ce848335fbbdd
Josefinita <mariajosefinadoffo@hotmail.com>
2014-09-25 22:47:00 -0300

    Update alignproteins.py

--- modules/alignproteins.py
+++ modules/alignproteins.py
@@ -1,35 +1,43 @@
-def _alignproteins(NM, codtable):
+def _Alignproteins(NM,codtable):
     
-    """T
-    This function translates a nucelotide matrix already  aligned
+    """This function translates a nucleotide matrix already  aligned.
     It goes along the rows and columns and list their elements, 
-    transforms them into a Biopython's sequence and eventually translate them to protein
+    transforms them into a Biopython's sequence and eventually translate them to protein.
+    Finally returns an aminoacidic matrix.
     
     Argument:
-    
     -NM- Nucleotide matrix
     -codtable- CodonTable from IUPAC
     
     Example:
-    
-    Alignproteinsmatrix=np.array([["A","C","G","G","A","A"],
-                                 ["A","C","G","T","C","T"]])
-    for i in range(0,len(Alignproteinsmatrix)):
-        list_1=Alineproteinsmatrix[i].tolist()
-        H=""
-        B=Ar.join(list_1)
-        Seq1=Bio.Seq.Seq(B)
-        return(Seq1.translate())
-        
-        TE
-        TS
-        """
-    from Bio.Data import CodonTable 
+    Alignproteinsmatrix=np.array([["A","C","T","G","C","A","C","G","A"],
+                                  ["T","G","A","A","C","G","A","G","G"]])
+                              from Bio.Data import CodonTable
     import numpy as np
     CodonTable.unambiguous_dna_by_id[codtable]
+    lista2=[]
+    for i in range (0,len(Alignproteinsmatrix)):
+        lista=[]
+        for j in range (0, len(Alignproteinsmatrix[0])-2,3):
+            H=""
+            B=H.join(Alignproteinsmatrix[i,j:j+3])
+            lista.append(Bio.Seq.translate(B,codtable))
+        lista2.append(lista)
+    return(np.array(lista2))
+        
+        array([['T', 'A', 'R'],
+             ['W', 'T', 'R']], 
+              dtype='|S1')
+        """
+    from Bio.Data import CodonTable
+    import numpy as np
+    CodonTable.unambiguous_dna_by_id[codtable]
+    lista2=[]
     for i in range (0,len(NM)):
-        list_1=NM[i].tolist()
-        H=""
-        B=H.join(list_1)
-        Seq1=Bio.Seq.Seq(B)
-        return(Seq1)
+        lista=[]
+        for j in range (0, len(NM[0])-2,3):
+            H=""
+            B=H.join(NM[i,j:j+3])
+            lista.append(Bio.Seq.translate(B,codtable))
+        lista2.append(lista)
+    return(np.array(lista2))
