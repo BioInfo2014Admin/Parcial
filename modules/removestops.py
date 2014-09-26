614d876b53c8c228e3d559fdd234dfde9933bb26
Tomás <tpachano@hotmail.com>
2014-09-25 22:41:33 -0300

    Update removestops.py

--- modules/removestops.py
+++ modules/removestops.py
@@ -1,4 +1,4 @@
-def _remove_stops(matrix,codontable_number):
+def _remove_stops(matrix,codontable_number,ID,stops):
     """
 
 This function removes sequences with premature stops from nucleotide matrix.
@@ -10,30 +10,44 @@ Argument:
 -codontable_number- genetic code id from NCBI
 
 Example:
-Alignedmatrix=np.array([["T","G","A","A","A","A","A","A","A","T","G","A"], ["A","A","A","A","A","A","T","G","A","A","A","A"], ["A","A","A","A","A","A","T","G","A","A","A","A"], ["T","G","A","A","A","A","T","G","A","A","A","A"]])
-ct = CodonTable.unambiguous_dna_by_id[11]
-    seqs_to_delete = []
-    counter = 0
-    for j in range(0,len(Alignedmatrix)):
-        for i in range(0,len(Alignedmatrix[0])-3,3):
-            codon_1=""
-            codon_2 = Alignedmatrix[j,i:i+3]
-            codon = codon_1.join(codon_2)
-            if codon in ct.stop_codons:
-                counter += 1
-            else:
-                pass
-            if counter > 1:
-                seqs_to_delete.append(j)
-                counter = 0
-            else:
-                pass
-        counter = 0
-    matrix_without_stops = np.delete(Alignedmatrix,seqs_to_delete,0)
-    return (matrix_without_stops)
 
-array([['A', 'A', 'A', 'A' , 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A'], ['A', 'A', 'A', 'A' , 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A']], 
+>>> import numpy as np
+>>> import Bio.Seq
+>>> from Bio.Data import CodonTable
+>>> arr = np.array([["T","G","A","A","A","A","A","A","A","T","G","A"], ["A","A","A","A","A","A","T","G","A","A","A","A"], ["A","A","A","A","A","A","T","G","A","A","A","A"], ["T","G","A","A","A","A","T","G","A","A","A","A"]])
+>>> arr
+array([['T', 'G', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'T', 'G', 'A'],
+       ['A', 'A', 'A', 'A', 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A'],
+       ['A', 'A', 'A', 'A', 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A'],
+       ['T', 'G', 'A', 'A', 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A']], 
       dtype='|S1')
+>>> codontable_number=11
+>>> ct = CodonTable.unambiguous_dna_by_id[codontable_number]
+>>> seqs_to_delete = []
+>>> counter = 0
+>>> for j in range(0,len(matrix)):
+...     for i in range(0,len(matrix[0]),3):
+...             codon_1 = ""
+...             codon_2 = matrix[j,i:i+3]
+...             codon_1.join(codon_2)
+...             if codon in ct.stop_codons:
+...                counter += 1
+...             else:
+...                pass
+...             if counter > 1:
+...                seqs_to_delete.append(j)
+...                counter = 0
+...             else:
+...                pass
+...     counter = 0
+...     matrix_without_stops = np.delete(matrix,seqs_to_delete,0)
+>>> print (seqs_to_delete)
+[0, 3]
+>>> matrix_without_stops
+matrix_without_stops( ['A', 'A', 'A', 'A', 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A'],
+                      ['A', 'A', 'A', 'A', 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A']], 
+                     dtype='|S1')
+
       
     """
     import numpy as np
@@ -58,4 +72,4 @@ array([['A', 'A', 'A', 'A' , 'A', 'A', 'T', 'G', 'A', 'A', 'A', 'A'], ['A', 'A',
                 pass
         counter = 0
     matrix_without_stops = np.delete(matrix,seqs_to_delete,0)
-    return (matrix_without_stops)
+    return (matrix_without_stops,ID)
