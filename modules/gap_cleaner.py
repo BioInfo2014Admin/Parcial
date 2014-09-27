6a14a0efb2e343ec4a5aa2f7a4b114758dbdddd3
gabybarros <gabrielaebarros@gmail.com>
2014-09-27 11:33:38 -0300

    Update gap_cleaner.py

--- modules/gap_cleaner.py
+++ modules/gap_cleaner.py
@@ -20,13 +20,17 @@ Example:
        ['A', 'C', 'C', 'A', 'A', 'T'])
 
 """
-    
+    def _gap_cleaner(matrix):
     import numpy as np
     gap_list = []
-               
-    for j in range(0,len(matrix)):
-        for i in range(0,len(matrix[0])):
-            if matrix[j,i]=='-':
-                gap_list.append(i)
+    flag="ok"
+    for i in range(0,len(matrix)):
+        for j in range(0,len(matrix[0])-2,3):
+            if "-" in matrix[i,j:j+3]:
+                gap_list.extend([j,j+1,j+2])
+                for k in matrix[i,j:j+3]:
+                    if k !="-":
+                        flag="warning"
     matrix_without_gaps = np.delete(matrix,gap_list,1)
     return (matrix_without_gaps)
+                        
